77b3bc674d9cd5db45a4269af301bfca74413d4c
Abdullah Khalid <abdullahkhalids@gmail.com>
2023-02-04 04:18:16 -0800

    bump python version for latest type hints

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/abdullahkhalids/stac",
+    python_requires=">=3.10",
     packages=find_packages(),
     include_package_data=True,
     install_requires=["ipython",
