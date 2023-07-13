738f3e3345391ffae455ea46ce0b3e9db8d3b3d7
Abdullah Khalid <abdullahkhalids@gmail.com>
2023-07-13 11:29:49 -0700

    Add initial implementation of color codes.

---
+++ stac/topologicalcodes/colorcode.py
@@ -0,0 +1,425 @@
+"""Module to provide color code."""
+from typing import Any
+from functools import partial
+import numpy as np
+import networkx as nx
+import bidict
+import matplotlib.pyplot as plt
+
+from stac import Circuit
+from ..code import Code
+from .primallattice import PrimalLattice
+
+
+class ColorCode(Code):
+    """Class for creating triangular color codes."""
+
+    def __init__(self,
+                 distance: int,
+                 geometry: str = "hexagonal",
+                 color_order: list[str] = ['g', 'r', 'b']
+                 ) -> None:
+        """
+        Construct the color code of some geometry and distance.
+
+        Parameters
+        ----------
+        distance : int
+            The distance of the code.
+        geometry : str, optional
+            Describes the shape of the primal lattice. The default and only
+            option currently is "hexagonal".
+        color_order: str, optional
+            Order of colors in the lattice.
+        """
+        self.color_order = color_order
+        # lattice length
+        L = int((distance-1)/2)
+
+        # rows of hexagons
+        rows = 3*L
+
+        # primal graph
+        self.primal_graph = nx.Graph()
+        self.primal_graph.faces = dict()
+        self.primal_graph.boundaries = {0: set(), 1: set(), 2: set()}
+        self.primal_graph.draw = self._primal_graph_draw
+
+        # determine nodes and edges of primal graph
+        self.primal_graph.nodes_index = bidict.bidict()
+        kk = 0
+        heights = [i for i in range(2, rows, 3)] + \
+            [i for i in range(rows + 1, 0, -3)]
+        row_nodes = [[] for i in range(rows+1)]
+        for i in range(distance):
+            for j in range(heights[i]):
+                self.primal_graph.add_node((i, j),
+                                           faces=set())
+                self.primal_graph.nodes_index[(i, j)] = kk
+                kk += 1
+                row_nodes[j].append((i, j))
+                if j != 0:
+                    self.primal_graph.add_edge((i, j), (i, j-1))
+
+        b = 0
+        for row in row_nodes:
+            for i in range(len(row)-1):
+                if b:
+                    self.primal_graph.add_edge(row[i], row[i+1])
+                b = (b+1) % 2
+            b = (b+1) % 2
+
+        # boundaries
+        self.primal_graph.boundaries[2] = \
+            set([(i, 0) for i in range(distance)])
+        self.primal_graph.boundaries[1] = \
+            set([row[0] for i, row in enumerate(row_nodes) if i % 3 in [0, 1]])
+        self.primal_graph.boundaries[0] = \
+            set([row[-1] for i, row in enumerate(row_nodes)
+                 if i % 3 in [0, 2]])
+
+        # faces
+        f_heights = [i+1 for i in range(2, 3*L, 3)] + \
+            [i+1 for i in range(3*L-2, 0, -3)]
+        self.primal_graph.faces_index = bidict.bidict()
+        kk = 0
+
+        c_bot = 0
+        c = 0
+        for i in range(2*L):
+            for j in range(c_bot, f_heights[i], 2):
+                self.primal_graph.faces[(i, j)] = dict()
+                if j == 0 and c == 0:
+                    self.primal_graph.faces[(i, j)]['form'] = 'bottom'
+                elif i < L and j == f_heights[i]-1:
+                    self.primal_graph.faces[(i, j)]['form'] = 'right'
+                elif i >= L and j == f_heights[i]-1:
+                    self.primal_graph.faces[(i, j)]['form'] = 'left'
+                else:
+                    self.primal_graph.faces[(i, j)]['form'] = 'full'
+
+                nodes = []
+
+                for di, dj in [(1, 1), (1, 0), (1, -1),
+                               (0, -1), (0, 0), (0, 1)]:
+                    if j+dj < 0 or j+dj >= heights[i+di]:
+                        continue
+                    nodes.append((i+di, j+dj))
+
+                self.primal_graph.faces[(i, j)]['nodes'] = nodes
+                for v in nodes:
+                    self.primal_graph.nodes[v]['faces'].add((i, j))
+
+                self.primal_graph.faces[(i, j)]['color'] = c
+                c = (c+2) % 3
+
+                self.primal_graph.faces_index[(i, j)] = kk
+                kk += 1
+
+            c_bot = (c_bot+1) % 2
+            c = c_bot
+
+        # add color to edges
+        all_boundary_nodes = self.primal_graph.boundaries[0] | \
+            self.primal_graph.boundaries[1] | \
+            self.primal_graph.boundaries[2]
+
+        # add color to all edges that terminate on boundaries
+        for j in range(3):
+            for v in self.primal_graph.boundaries[j]:
+                for e in self.primal_graph.edges(v):
+                    if e[1] not in all_boundary_nodes:
+                        self.primal_graph.edges[e]['color'] = j
+        # add color to edges from corner
+        self.primal_graph.edges[(L, rows), (L, rows-1)]['color'] = 1
+        self.primal_graph.edges[(0, 0), (0, 1)]['color'] = 2
+        self.primal_graph.edges[(2*L, 0), (2*L-1, 0)]['color'] = 0
+
+        # add color to edges in bulk
+        for e in self.primal_graph.edges:
+            if 'color' in self.primal_graph.edges[e]:
+                continue
+            face = self.primal_graph.nodes[e[0]]['faces'].difference(
+                self.primal_graph.nodes[e[1]]['faces']).pop()
+            self.primal_graph.edges[e]['color'] = \
+                self.primal_graph.faces[face]['color']
+
+        # corner colors
+        self.primal_graph.corners = dict()
+        self.primal_graph.corners[0] = (0, 0)
+        self.primal_graph.corners[1] = (2*L, 0)
+        self.primal_graph.corners[2] = (L, 3*L)
+
+        self.primal_lattice = PrimalLattice(distance,
+                                            self.primal_graph,
+                                            color_order)
+
+        # create the generator matrix
+        n = int(3*(distance-1)*(distance+1)/4+1)
+        mhalf = int((n-1)/2)
+        H = np.zeros((mhalf, n), dtype=int)
+
+        for i, f in self.primal_graph.faces_index.inv.items():
+            for v in self.primal_graph.faces[f]['nodes']:
+                v_ind = self.primal_graph.nodes_index[v]
+                H[i, v_ind] = 1
+
+        generator_matrix = np.zeros((2*mhalf, 2*n), dtype=int)
+        generator_matrix[:mhalf, :n] = H
+        generator_matrix[mhalf:, n:] = H
+
+        super().__init__(generator_matrix)
+        self.distance = distance
+
+    def construct_logical_operators(self,
+                                    method: str = "boundary: blue"
+                                    ) -> (Any, Any):
+        """
+        Construct logical operators of the code.
+
+        Parameters
+        ----------
+        method : str, optional
+            With boundaries with color 0, 1, 2. The options are:
+                "boundary: green"
+                "boundary: red"
+                "boundary: blue" (default)
+                "gottesman" (generic method)
+
+        Returns
+        -------
+        logical_xs: numpy.array
+            Array of logical xs. Each row is an operator.
+        logical_zs: numpy.array
+            Array of logical xs. Each row is an operator.
+        """
+        if method == "boundary: green":
+            c = self.primal_lattice.color_order.index('g')
+        elif method == "boundary: red":
+            c = self.primal_lattice.color_order.index('r')
+        elif method == "boundary: blue":
+            c = self.primal_lattice.color_order.index('b')
+        else:
+            return super().construct_logical_operators(method)
+
+        oper_x = np.zeros(2*self.num_data_qubits, dtype=int)
+        oper_z = np.zeros(2*self.num_data_qubits, dtype=int)
+        for node in self.primal_graph.boundaries[c]:
+            oper_x[self.primal_graph.nodes_index[node]] = 1
+            oper_z[self.num_data_qubits +
+                   self.primal_graph.nodes_index[node]] = 1
+
+        self.logical_xs = np.array([oper_x])
+        self.logical_zs = np.array([oper_z])
+
+        return self.logical_xs, self.logical_zs
+
+    def _primal_graph_draw(self,
+                           draw_face_labels: bool = True
+                           ) -> None:
+        """
+        Draw the primal graph.
+
+        Parameters
+        ----------
+        draw_face_labels : bool, optional
+            Draw the face labels. The default is True.
+        """
+        # nicer labels
+        if not hasattr(self.primal_graph, '_node_labels'):
+            self.primal_graph._node_labels = dict()
+            for node in self.primal_graph.nodes:
+                self.primal_graph._node_labels[node] = str(node)[1:-1]
+
+        nx.draw(self.primal_graph,
+                pos=nx.get_node_attributes(self.primal_graph, 'pos_graph'),
+                node_size=450,
+                font_size=7,
+                labels=self.primal_graph._node_labels,
+                with_labels=True)
+
+        if draw_face_labels:
+            # nicer labels
+            if not hasattr(self.primal_graph, '_face_labels'):
+                self.primal_graph._face_labels = dict()
+                for face in self.primal_graph.faces:
+                    self.primal_graph._face_labels[face] = str(face)[1:-1]
+
+            plt.axis('off')
+            pos = {f: val['pos_graph']
+                   for f, val in self.primal_graph.faces.items()}
+
+            nx.draw_networkx_labels(self.primal_graph,
+                                    pos=pos,
+                                    labels=self.primal_graph._face_labels,
+                                    font_size=7)
+
+    def construct_dual_graph(self):
+        """
+        Construct the dual graph of the code.
+
+        In the dual graph, the stabilizers are mapped onto the vertices and
+        the qubits are mapped onto the faces. The stabilizers refer to either
+        the set of pure X stabilizers of the code, or the pure Z ones. The
+        vertices are colored, like the faces of the primal lattice.
+        """
+        self.dual_graph = nx.Graph()
+        # nodes of dual graph are faces of primal
+        for face, val in self.primal_graph.faces.items():
+            pos = (-self.primal_lattice.x_shift + val['pos_lat'][0],
+                   self.primal_lattice.y_shift - val['pos_lat'][1])
+            self.dual_graph.add_node(face,
+                                     color=val['color'],
+                                     pos_graph=pos)
+
+        # now add edges between the new nodes
+        for face in self.primal_graph.faces:
+            for d in [(-1, -1), (-1, 1), (0, -2), (0, 2), (1, -1), (1, 1)]:
+                connected_face = (face[0] + d[0], face[1] + d[1])
+                if connected_face in self.primal_graph.faces:
+                    self.dual_graph.add_edge(face, connected_face)
+
+        # color of nodes has to be a list for draw function
+        self.dual_graph.node_colors = \
+            [self.primal_lattice.color_map[self.primal_lattice.color_order[c]]
+             for f, c in nx.get_node_attributes(
+                self.dual_graph, 'color').items()]
+
+        # now add faces, which are triangles
+        self.dual_graph.faces = dict()
+        self.dual_graph._face_labels = dict()
+        for node in self.primal_graph.nodes:
+            self.dual_graph.faces[node] = dict()
+            self.dual_graph.faces[node]['nodes'] = \
+                self.primal_graph.nodes[node]['faces']
+            self.dual_graph.faces[node]['pos_graph'] = \
+                self.primal_graph.nodes[node]['pos_graph']
+            self.dual_graph._face_labels[node] = str(node)[1:-1]
+
+        # nicer labels
+        self.dual_graph._node_labels = dict()
+        for node in self.dual_graph.nodes:
+            self.dual_graph._node_labels[node] = str(node)[1:-1]
+        # draw
+        self.dual_graph.draw = self._dual_graph_draw
+
+    def _dual_graph_draw(self,
+                         draw_vertex_labels: bool = True,
+                         draw_face_labels: bool = True):
+        """
+        Draw the dual graph.
+
+        Parameters
+        ----------
+        draw_vertex_labels : bool, optional
+            Draw the vertex labels. The default is True.
+        draw_face_labels : bool, optional
+            Draw the face labels. The default is False.
+        """
+        nx.draw(self.dual_graph,
+                pos=nx.get_node_attributes(self.dual_graph, 'pos_graph'),
+                node_color=self.dual_graph.node_colors,
+                node_size=450,
+                font_size=7,
+                labels=self.dual_graph._node_labels,
+                with_labels=True)
+
+        if draw_face_labels:
+            plt.axis('off')
+            pos = {f: val['pos_graph']
+                   for f, val in self.dual_graph.faces.items()}
+            nx.draw_networkx_nodes(self.primal_graph,
+                                   pos=pos,
+                                   node_color='white')
+            nx.draw_networkx_labels(self.primal_graph,
+                                    pos=pos,
+                                    labels=self.dual_graph._face_labels,
+                                    font_size=7)
+
+    def construct_restricted_graphs(self):
+        """
+        Construct the restricted graphs.
+
+        There are three restricted graphs. Each is built by omitting vertices
+        of one color from the dual graph.
+
+        The graphs are stored in the dictionary `self.restricted_graphs`. There
+        are three keys for this dictionary, (0, 1), (0, 2), and (1, 2),
+        referring to the colors that are included in the graph.
+        """
+        self.restricted_graphs = dict()
+        for c1, c2 in [(0, 1), (0, 2), (1, 2)]:
+            self.restricted_graphs[c1, c2] = nx.Graph()
+
+            for node, val in self.dual_graph.nodes.items():
+                if val['color'] in [c1, c2]:
+                    self.restricted_graphs[c1, c2].add_node(node, **val)
+
+            for node in self.dual_graph.nodes:
+                if self.dual_graph.nodes[node]['color'] != c1:
+                    continue
+                for e in nx.edges(self.dual_graph, node):
+                    if self.dual_graph.nodes[e[1]]['color'] == c2:
+                        self.restricted_graphs[c1, c2].add_edge(*e)
+                        self.restricted_graphs[c1, c2].edges[e]['faces'] = \
+                            set(self.primal_graph.faces[e[0]]['nodes']) & \
+                            set(self.primal_graph.faces[e[1]]['nodes'])
+
+            self.restricted_graphs[c1, c2].node_colors = \
+                [self.primal_lattice.color_map[
+                    self.primal_lattice.color_order[c]]
+                 for f, c in nx.get_node_attributes(
+                    self.restricted_graphs[c1, c2], 'color').items()]
+
+            self.restricted_graphs[c1, c2].draw = partial(
+                self._restricted_graph_draw, (c1, c2))
+            self.restricted_graphs[c1, c2].draw.__doc__ = \
+                """\
+                Draw this restricted graph.
+
+                Parameters
+                ----------
+                draw_edge_labels : bool, optional
+                    Draw the edge labels. The default is False.
+                """
+
+    def _restricted_graph_draw(self,
+                               label: tuple,
+                               draw_edge_labels: bool = False):
+        """
+        Draw a restricted graph.
+
+        Parameters
+        ----------
+        label : tuple
+            The label of the restricted graph: (0, 1), (0, 2) or (1, 2).
+        draw_edge_labels : bool, optional
+            Draw the edge labels. The default is False.
+        """
+        node_labels = dict()
+        for node in self.restricted_graphs[label].nodes:
+            node_labels[node] = str(node)[1:-1]
+
+        pos = nx.get_node_attributes(
+            self.restricted_graphs[label], 'pos_graph')
+
+        nx.draw(self.restricted_graphs[label],
+                pos=pos,
+                node_color=self.restricted_graphs[label].node_colors,
+                node_size=450,
+                font_size=7,
+                labels=node_labels,
+                with_labels=True)
+
+        if draw_edge_labels:
+            edge_labels = dict()
+
+            for e, val in self.restricted_graphs[label].edges.items():
+                fs = list(val['faces'])
+                edge_labels[e] = str(fs[0])[1:-1] + '\n' + str(fs[1])[1:-1]
+
+            nx.draw_networkx_edge_labels(self.restricted_graphs[label],
+                                         pos=pos,
+                                         edge_labels=edge_labels,
+                                         font_size=7,
+                                         verticalalignment='center_baseline')
---
+++ stac/topologicalcodes/primallattice.py
@@ -0,0 +1,290 @@
+"""Module to provide primal lattice for color codes."""
+from typing import Optional
+import numpy as np
+import svg
+from IPython.display import display, SVG
+
+
+class PrimalLattice:
+    """Primal lattice for color codes."""
+
+    def __init__(self,
+                 distance,
+                 primal_graph,
+                 color_order):
+        self.color_order = color_order
+        self.primal_graph = primal_graph
+        self.params = dict()
+        # lattice length
+        self.lattice_length = int((distance-1)/2)
+
+        # cols and rows in the grid
+        self.face_cols = 2*self.lattice_length
+        self.face_rows = 3*self.lattice_length
+
+        # size of hexagon
+        self.hexagon_size = 40
+
+        # horizontal separation between hexagons
+        self.hor_sep = self.hexagon_size*3/2
+        # vertical separation between centers of diagonally stacked hexagons
+        self.ver_sep = self.hexagon_size*np.sqrt(3)/2
+
+        # shift grid to keep in svg frame
+        self.x_shift = self.hexagon_size*2
+        self.y_shift = 3*self.lattice_length*self.ver_sep + self.hexagon_size/2
+
+        # color map
+        self.color_map = {'r': '#FA8072', 'g': '#33FF93', 'b': '#069AF3'}
+
+        # face coordinates
+        for f in primal_graph.faces:
+            x = self.hexagon_size + self.hor_sep*f[0]
+            y = self.ver_sep*f[1]
+            primal_graph.faces[f]['pos_graph'] = (x, y)
+            primal_graph.faces[f]['pos_lat'] = \
+                (self.x_shift + x,
+                 self.y_shift - y)
+
+        # vertex coordinates
+        def xe(i):
+            return self.hexagon_size * (-1)**i * (-1 + (-1)**i * (1 + 6*i))/4
+        for node in primal_graph.nodes:
+            x = xe(node[0])
+            if node[1] % 2 == 1:
+                x = x + self.hexagon_size/2 if node[0] % 2 == 0 \
+                    else x - self.hexagon_size/2
+            y = node[1] * self.ver_sep
+            primal_graph.nodes[node]['pos_graph'] = (x, y)
+            primal_graph.nodes[node]['pos_lat'] = \
+                (self.x_shift + x, self.y_shift - y)
+
+    def _create_face_svg(self,
+                         face):
+        """
+        Create an svg.Polygon object for a face.
+
+        Parameters
+        ----------
+        x0 : float
+            Horizontal position of center.
+        y0 : float
+            Vertical position of center.
+        color : str
+            Options are r, g or b.
+
+        Returns
+        -------
+        pg : svg.Polygon
+            The svg.Polygon object of the hexagon.
+
+        """
+        coords = [self.primal_graph.nodes[node]['pos_lat']
+                  for node in self.primal_graph.faces[face]['nodes']]
+
+        pts = [a for p in coords for a in p]
+
+        c = self.color_order[self.primal_graph.faces[face]['color']]
+
+        pg = svg.Polygon(
+            points=pts,
+            fill=self.color_map[c],
+            stroke="black",
+            stroke_width=1,
+            stroke_linejoin="round",
+        )
+        return pg
+
+    def setup_draw(self,
+                   draw_boundaries: bool = False,
+                   draw_vertex_labels: Optional[int] = None,
+                   draw_face_labels: Optional[int] = None
+                   ) -> None:
+        """
+        Set the options for drawing the primal lattice.
+
+        The `draw` function can be used to display the lattice.
+
+        Parameters
+        ----------
+        draw_boundaries : bool, optional
+            Draw the boundaries of the lattice. The default is False.
+        draw_vertex_labels : Optional[int], optional
+            Draw the vertex labels. The default is None.
+        draw_face_labels : Optional[int], optional
+            Draw the face labels. The default is None.
+        """
+        self._svg_els = []
+
+        # draw the boundaries first
+        if draw_boundaries:
+            right_corner_x = self.face_cols * self.hor_sep + self.x_shift
+            pts = [self.x_shift, self.y_shift+5,
+                   self.x_shift, self.y_shift,
+                   right_corner_x, self.y_shift,
+                   right_corner_x, self.y_shift+5]
+            # bottom boundary
+            self._svg_els.append(
+                svg.Polygon(
+                    points=pts,
+                    fill='blue',
+                    stroke="black",
+                    stroke_width=1,
+                    stroke_linejoin="round",
+
+                ))
+            # left boundary
+            self._svg_els.append(
+                svg.Polygon(
+                    points=pts,
+                    fill='red',
+                    stroke="black",
+                    stroke_width=1,
+                    stroke_linejoin="round",
+                    transform=f'rotate(-60 {self.x_shift-5} {self.y_shift})'
+                ))
+            # right boundary
+            self._svg_els.append(
+                svg.Polygon(
+                    points=pts,
+                    fill='green',
+                    stroke="black",
+                    stroke_width=1,
+                    stroke_linejoin="round",
+                    transform=f'rotate(60 {right_corner_x+5} {self.y_shift})'
+                ))
+        for face in self.primal_graph.faces:
+            self._svg_els.append(
+                self._create_face_svg(face)
+            )
+
+        if type(draw_vertex_labels) is int:
+            face_color = [0, 1, 2] if draw_vertex_labels == 3 \
+                else [draw_vertex_labels]
+            included_faces = [f for f in self.primal_graph.faces
+                              if self.primal_graph.faces[f]['color']
+                              in face_color]
+            for face in included_faces:
+                for node in self.primal_graph.faces[face]['nodes']:
+                    node_pos = self.primal_graph.nodes[node]['pos_lat']
+                    self._svg_els.append(
+                        svg.Text(x=node_pos[0], y=node_pos[1],
+                                 text=f'{node[0]},{node[1]}',
+                                 font_size=8,
+                                 text_anchor='middle'))
+
+        if type(draw_face_labels) is int:
+            face_color = [0, 1, 2] if draw_face_labels == 3 \
+                else [draw_face_labels]
+            included_faces = [f for f in self.primal_graph.faces
+                              if self.primal_graph.faces[f]['color']
+                              in face_color]
+            for face in included_faces:
+                face_pos = self.primal_graph.faces[face]['pos_lat']
+                self._svg_els.append(svg.Text(x=face_pos[0], y=face_pos[1],
+                                              text=f'{face[0]},{face[1]}',
+                                              font_size=10,
+                                              text_anchor='middle'))
+
+    def label_vertex(self,
+                     label: str,
+                     node: tuple) -> None:
+        """
+        Label a vertex on the lattice to be drawn.
+
+        Parameters
+        ----------
+        label : str
+            Label to include. One character for nice display.
+        node : tuple
+            The address of the node at which to place the label..
+        """
+        node_pos = self.primal_graph.nodes[node]['pos_lat']
+        self._svg_els.append(
+            svg.Circle(cx=node_pos[0], cy=node_pos[1],
+                       r=8,
+                       fill='white',
+                       stroke=None))
+        self._svg_els.append(
+            svg.Text(x=node_pos[0], y=node_pos[1]+2.5,
+                     text=label,
+                     font_size=10,
+                     text_anchor='middle'))
+
+    def label_operator(self,
+                       operator: np.ndarray
+                       ) -> None:
+        """
+        Label an operator on the lattice to be drawn.
+
+        Parameters
+        ----------
+        operator : np.ndarray
+            A one-dimensional numpy array of the operator. with length twice
+            the number of qubits in the code. Entries should be 0 or 1.
+        """
+
+        n = int(len(operator)/2)
+        for i in range(n):
+            if operator[i] and not operator[n+i]:
+                self.label_vertex('X', self.primal_graph.nodes_index.inv[i])
+            elif operator[i] and operator[n+i]:
+                self.label_vertex('Y', self.primal_graph.nodes_index.inv[i])
+            elif not operator[i] and operator[n+i]:
+                self.label_vertex('Z', self.primal_graph.nodes_index.inv[i])
+
+    def label_face(self,
+                   label: str,
+                   face: tuple
+                   ) -> None:
+        """
+        Label a face on the lattice to be drawn.
+
+        Parameters
+        ----------
+        label : str
+            The string to be placed on the face.
+        face : tuple
+            The address of the face which is to be labelled.
+        """
+        node_pos = self.primal_graph.faces[face]['pos_lat']
+        self._svg_els.append(
+            svg.Rect(x=node_pos[0]-5, y=node_pos[1]-5,
+                     width=10, height=10,
+                     fill='white',
+                     stroke=None))
+        self._svg_els.append(
+            svg.Text(x=node_pos[0], y=node_pos[1]+2.5,
+                     text=label,
+                     font_size=10,
+                     text_anchor='middle'))
+
+    def label_syndrome(self,
+                       syndrome: np.ndarray
+                       ) -> None:
+        """
+        Label a syndrome on the lattice to be drawn.
+
+        Parameters
+        ----------
+        syndrome : np.ndarray
+            A one-dimensional numpy array. Length should be equal to the number
+            of generators of the code. Entries should be 0 or 1.
+        """
+        m = int(len(syndrome)/2)
+        for i in range(m):
+            if syndrome[i] and not syndrome[m+i]:
+                self.label_face('X', self.primal_graph.faces_index.inv[i])
+            elif syndrome[i] and syndrome[m+i]:
+                self.label_face('Y', self.primal_graph.faces_index.inv[i])
+            elif not syndrome[i] and syndrome[m+i]:
+                self.label_face('Z', self.primal_graph.faces_index.inv[i])
+
+    def draw(self
+             ) -> None:
+        """Display the primal lattice with any labels put on it."""
+        img = svg.SVG(
+            width=self.face_cols*self.hor_sep + 3*self.hexagon_size,
+            height=self.face_rows*self.ver_sep + 1*self.hexagon_size,
+            elements=self._svg_els)
+        display(SVG(img.as_str()))
