b74c15f559e79b6d89c062e371a18575c977bad6
Abdullah Khalid <abdullahkhalids@gmail.com>
2023-08-16 16:36:31 -0700

    Add CAT operation.

--- stac/supportedoperations.py
+++ stac/supportedoperations.py
@@ -10,16 +10,20 @@ num_targets: int
 
 _operations = dict()
 
-for name in ['I', 'X', 'Y', 'Z', 'H', 'S', 'T']:
+for name in ['I', 'X', 'Y', 'Z', 'H', 'S', 'T', 'CAT']:
     _operations[name] = {
         'ins_type': 0,
         'is_parameterized': False,
         'num_targets': 1,
         'draw_text': [name],
         'draw_img': [name],
+        'stim_str': name,
         'qasm_str': name.lower() + ' q[{t0}];\n'
         }
 _operations['I']['qasm_str'] = 'id q[{t0}];\n'
+_operations['CAT']['stim_str'] = 'H'
+_operations['CAT']['draw_text'] = 'H'
+_operations['CAT']['draw_img'] = 'H'
 
 for name in ['RX', 'RY', 'RZ']:
     _operations[name] = {
@@ -29,6 +33,7 @@ for name in ['RX', 'RY', 'RZ']:
         'num_parameters': 1,
         'draw_text': [name],
         'draw_img': [name],
+        'stim_str': name,
         'qasm_str': name.lower() + '({p0}) q[{t0}];\n'
         }
 
@@ -39,10 +44,11 @@ for name in ['CX', 'CY', 'CZ']:
         'num_targets': 2,
         'draw_text': ['●', name[1]],
         'draw_img': ['●', name[1]],
+        'stim_str': name,
         'qasm_str': name.lower() + ' q[{t0}],q[{t1}];\n'
         }
 _operations['CX']['draw_text'][1] = '⊕'
-_operations['CZ']['draw_text'][0] = '●'
+_operations['CZ']['draw_text'][1] = '●'
 
 for name in ['R', 'M', 'MR']:
     _operations[name] = {
@@ -51,6 +57,7 @@ for name in ['R', 'M', 'MR']:
         'num_targets': 1,
         'draw_text': [name],
         'draw_img': [name],
+        'stim_str': name,
         'qasm_str': ''
         }
 _operations['MR']['draw_text'][0] = 'm'
@@ -64,5 +71,7 @@ for name in ['TICK']:
         'ins_type': 1,
         'is_parameterized': False,
         'num_targets': 0,
+        'stim_str': name,
+        'qasm_str': ''
         }
 _operations['TICK']['qasm_str'] = 'barrier q;\n'
