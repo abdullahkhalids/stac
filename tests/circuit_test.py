d84cc4863e96077ecfa1ba061b291fc9e2b751ca
Abdullah Khalid <abdullahkhalids@gmail.com>
2023-09-01 19:24:04 -0700

    Add test for simple circuit init.

--- tests/circuit_test.py
+++ tests/circuit_test.py
@@ -3,3 +3,8 @@ import stac
 def test_circuit_init():
     circ = stac.Circuit()
     assert type(circ) == stac.circuit.Circuit
+
+
+def test_circuit_init2():
+    circ = stac.Circuit.simple(3)
+    assert type(circ) == stac.circuit.Circuit
