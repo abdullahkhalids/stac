57c6e03772831b599dc98a78b7d3fa77c2fc4d74
Abdullah Khalid <abdullahkhalids@gmail.com>
2023-09-05 21:48:06 -0700

    Specify imported names instead of *.

--- stac/__init__.py
+++ stac/__init__.py
@@ -1,13 +1,15 @@
 """Initiatization for stac."""
 
-from .qubit import *
-from .register import *
-from .operation import *
-from .timepoint import *
-from .circuit import *
-from .code import *
+from .qubit import PhysicalQubit, VirtualQubit
+from .register import Register, QubitRegister, RegisterRegister
+from .operation import Operation
+from .timepoint import Timepoint
+from .annotation import Annotation, AnnotationSlice
+from .measurementrecord import MeasurementRecord
+from .supportedinstructions import instructions
+from .circuit import Circuit
+from .code import print_matrix, print_paulis, print_paulis_indexed, Code
 from .commoncodes import CommonCodes
-from .concatenation import *
+from .concatenation import ConcatCode
 from .instructionblock import InstructionBlock
 from .topologicalcodes.colorcode import ColorCode
-
